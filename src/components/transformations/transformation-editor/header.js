b671f3d5fd8a0bef9746715a49b044e308e8a4c2
Tamir Revivi <tamirrevivi09@gmail.com>
2020-02-29 14:02:31 +0200

    remove unused makeStyles

--- src/components/transformations/transformation-editor/header.js
+++ src/components/transformations/transformation-editor/header.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import ToolBar from '@material-ui/core/ToolBar';
 import Typography from '@material-ui/core/Typography';
