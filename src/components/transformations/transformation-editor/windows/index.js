66a348cda3f9bcc979719f6440a20bc0c732e242
Tamir Revivi <tamirrevivi09@gmail.com>
2020-02-29 22:19:43 +0200

    remove unused import in windows

--- src/components/transformations/transformation-editor/windows/index.js
+++ src/components/transformations/transformation-editor/windows/index.js
@@ -1,12 +1,5 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import ToolBar from '@material-ui/core/ToolBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import Close from '@material-ui/icons/Close';
 import WindowSelector from './window-selector';
 import VariablePane from './variable-pane';
 
