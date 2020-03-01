52b31b8e422428ee231e8bb8d19f672dd417224b
Tamir Revivi <tamirrevivi09@gmail.com>
2020-03-02 00:08:05 +0200

    variable list only overflows in y axis

--- src/components/transformations/transformation-editor/object-selector/variables.js
+++ src/components/transformations/transformation-editor/object-selector/variables.js
@@ -22,6 +22,7 @@ const useStyles = makeStyles(theme => ({
     boxShadow: '0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12)',
     padding: 0,
     overflowY: 'auto',
+    overflowX: 'hidden',
     width: '15vw',
     zIndex: 2,
     '& .MuiListItemAvatar-root': {
