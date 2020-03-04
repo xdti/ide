28c1dc9e3386bb0abc59d0881cc81d23820ff886
Tamir Revivi <tamirrevivi09@gmail.com>
2020-03-04 23:43:43 +0200

    fix config key to label

--- src/components/transformations/transformation-editor/windows/general-config-pane.js
+++ src/components/transformations/transformation-editor/windows/general-config-pane.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import capitalize from 'lodash/capitalize';
+import startCase from 'lodash/startCase';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Add from '@material-ui/icons/Add';
@@ -61,7 +61,7 @@ export default function GeneralConfigPane(props) {
             <TextField
               className={classes.maxWidthInput}
               variant="outlined"
-              label={capitalize(k)}
+              label={startCase(k)}
               value={config[k]}
               onChange={(e) => props.update({ [k]: e.target.value })}
             />
