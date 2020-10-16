ab6f514607bb6868f6c7d34cf168be3787d2acf1
Tamir Revivi <tamirrevivi09@gmail.com>
2020-10-17 01:32:40 +0300

    move map overflow to generic folder

--- src/components/transformations/transformation-list/transformation/extra-details.js
+++ src/components/transformations/transformation-list/transformation/extra-details.js
@@ -5,7 +5,7 @@ import Edit from '@material-ui/icons/Edit';
 import Delete from '@material-ui/icons/Delete';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import MapOverflow from 'components/map-overflow';
+import MapOverflow from 'components/generic/map-overflow';
 
 const useStyles = makeStyles(theme => ({
    container: {
