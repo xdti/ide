9c5973c0acbf9e9bca195e97b3fe0aa0a914c134
Tamir Revivi <tamirrevivi09@gmail.com>
2020-10-16 21:34:44 +0300

    remove console logs

--- src/completers/templates.js
+++ src/completers/templates.js
@@ -3,10 +3,8 @@ export default (templates, suffix) => ({
   identifierRegexps: [/[^\s]+/],
   getCompletions: (editor, session, pos, prefix, callback) => {
     let current = editor.container.id;
-    console.log(current);
     let templateNames = Object.values(templates || {})
       .map(v => v.name);
-    console.log(templateNames);
     let isTemplate = templateNames.includes(current);
     callback(
       null,
